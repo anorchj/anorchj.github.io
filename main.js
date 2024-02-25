bce4283738433c0abdf02e5429b6eb98e0784459
AJ <anorch-10@hotmail.com>
2024-02-25 14:27:21 -0500

    Lab 5

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ function imgFunction() {
     document.getElementById("myImg").width = "300";
 }
 
+//When the user clicks on the button, toggle between hiding and showing the drop down content
+function menuFunction() {
+    document.getElementById("myDropdown").classList.toggle("show"); 
+}
+//Close the drop down menu if the user clicks outside of it 
+window.onclick=function(event) { 
+    if (!event.target.matches('.dropbtn')) { 
+        var dropdowns=document.getElementsByClassName("dropdown-content"); 
+        for(var i=0; i<dropdowns.length; i++){ varopenDropdown=dropdowns[i]; 
+            if (openDropdown.classList.contains('show')){ 
+                openDropdown.classList.remove('show'); 
+            } 
+        } 
+    } 
+}
