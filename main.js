079c669fee9a0d8b1d799dd2c7d4a4dd9f169e70
AJ <anorch-10@hotmail.com>
2024-02-25 12:29:07 -0500

    Lab 5

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+//Function for name prompt and display of output as html element
 function myFunction() {
-    let person = prompt("Please enter your name");
+    var person = prompt("Please enter your name");
     if (person != null) {
       document.getElementById("demo").innerHTML =
       person;
